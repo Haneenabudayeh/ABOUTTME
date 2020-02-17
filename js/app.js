9590cc9962321e4fac5d3f93b40bb987b4c5f3d1
haneenabudayeh <haneenabudayeh5@gmail.com>
2020-02-17 19:42:09 +0200

    lab 2

---
+++ js/app.js
@@ -0,0 +1,87 @@
+'use strict'
+
+
+var name = prompt('what is your name ?');
+console.log('Name', name);
+alert('welcome' + name);
+
+var Q1 = prompt('Do you think my age is 24?')
+switch(Q1.toLocaleLowerCase()){
+
+    case 'yes':
+        case'y':
+        alert('yes, thats true');
+        console.log('do you think my age is 24?');
+        break;
+        case 'no':
+        case 'n':
+alert('no , thats fulse')
+default:
+    breack;
+}
+
+
+
+var Q2 = prompt('Do you think i love coffe?')
+switch(Q2.toLocaleLowerCase()){
+
+    case 'yes':
+        case'y':
+        alert('yes, thats true');
+        console.log('Do you think i love coffe?');
+        break;
+        case 'no':
+        case 'n':
+alert('no , thats fulse')
+default:
+    breack;
+}
+
+var Q3 = prompt('do you think the best food is potato?')
+switch(Q3.toLocaleLowerCase()){
+
+    case 'yes':
+        case'y':
+        alert('no , thats fulse')
+        console.log('Do you think the best food is potato?');
+        break;
+        case 'no':
+        case 'n':
+            alert('yes, thats true');
+default:
+    breack;
+}
+
+
+var Q4 = prompt('do you think i love cat?')
+switch(Q4.toLocaleLowerCase()){
+
+    case 'yes':
+        case'y':
+        alert('no , thats fulse')
+        console.log('do you think i love cat?');
+        break;
+        case 'no':
+        case 'n':
+            alert('yes, thats true');
+default:
+    breack;
+}
+
+
+var Q5 = prompt('do you think i have a car?')
+switch(Q5.toLocaleLowerCase()){
+
+    case 'yes':
+        case'y':
+        alert('no , thats fulse')
+        console.log('do you think i have a car');
+        break;
+        case 'no':
+        case 'n':
+            alert('yes, thats true');
+default:
+    breack;
+}
+
+document.write("<h1>welcome in your store page !</h1><p>Have a nice day!</p>");
